29449dcadd72d50598ef07d3ad1ec9cbad9a5231
Bridger Tower <bridgertower@gmail.com>
2023-06-08 10:22:16 -0600

    Update

--- app/page.js
+++ app/page.js
@@ -13,21 +13,45 @@ export default function Home() {
 	return (
 		<main>
 			<Section>
-				<Link href="/" className='mb-12 block'>
+				<Link href="/" className="mb-12 block">
 					<Image src={Cap} alt="bridger tower key cap" width={75} className="dark:invert" />
 				</Link>
 				<h1 className="text-3xl">Bridger Tower</h1>
 				<h2>
 					Designer and Developer from Salt Lake City, UT. Crafting software and websites using{' '}
-					<a className="transition-all border-b border-black dark:border-white hover:pb-1 hover:-mt-1 hover:border-blue-500"
-						href="https://figma.com">Figma</a>, {" "}<a className="transition-all border-b border-black dark:border-white hover:pb-1 hover:-mt-1 hover:border-blue-500"
-							href="https://nextjs.org">Next.js</a>,{' '}
-					<a className="transition-all border-b border-black dark:border-white hover:pb-1 hover:-mt-1 hover:border-blue-500"
-						href="https://tailwindcss.com">Tailwind</a>, and{' '}
-					<a className="transition-all border-b border-black dark:border-white hover:pb-1 hover:-mt-1 hover:border-blue-500"
-						href="https://vercel.com">Vercel</a>.{' '}
+					<a
+						className="transition-all border-b border-black dark:border-white hover:pb-1 hover:-mt-1 hover:border-blue-500"
+						href="https://figma.com"
+					>
+						Figma
+					</a>
+					,{' '}
+					<a
+						className="transition-all border-b border-black dark:border-white hover:pb-1 hover:-mt-1 hover:border-blue-500"
+						href="https://nextjs.org"
+					>
+						Next.js
+					</a>
+					,{' '}
+					<a
+						className="transition-all border-b border-black dark:border-white hover:pb-1 hover:-mt-1 hover:border-blue-500"
+						href="https://tailwindcss.com"
+					>
+						Tailwind
+					</a>
+					, and{' '}
+					<a
+						className="transition-all border-b border-black dark:border-white hover:pb-1 hover:-mt-1 hover:border-blue-500"
+						href="https://vercel.com"
+					>
+						Vercel
+					</a>
+					.{' '}
 				</h2>
-				<h3>As a creative technologist, I combine my passions for design and code to create timeless brands, impactful digital products, and captivating websites.</h3>
+				<h3>
+					As a creative technologist, I combine my passions for design and code to create timeless
+					brands, impactful digital products, and captivating websites.
+				</h3>
 			</Section>
 
 			<Section>
@@ -51,11 +75,11 @@ export default function Home() {
 				<h3 className="text-xl">Current Ventures</h3>
 				{Projects.map((project, index) => (
 					<a href={project.href} key={index} className="md:flex">
-						<div className='md:w-48 flex items-baseline gap-2 mb-2 mt-4'>
-							<h4 className='text-lg'>{project.title}</h4>
-							<p className='text-sm opacity-75'>{project.linkName}</p>
+						<div className="md:w-48 flex items-baseline gap-2 mb-2 mt-4">
+							<h4 className="text-lg">{project.title}</h4>
+							<p className="text-sm opacity-75">{project.linkName}</p>
 						</div>
-						<div className='w-full flex gap-2'>
+						<div className="w-full flex gap-2">
 							<p>&rarr;</p>
 							<p>{project.description}</p>
 						</div>
@@ -78,8 +102,14 @@ export default function Home() {
 					Send me an email<a href="mailto:bridgertower@gmail.com">here.</a>
 				</p>
 				<p>
-					Created by <a className="transition-all border-b border-black dark:border-white hover:pb-1 hover:-mt-1 hover:border-blue-500"
-						href="https://github.com/brijr">Bridger Tower</a>.
+					Created by{' '}
+					<a
+						className="transition-all border-b border-black dark:border-white hover:pb-1 hover:-mt-1 hover:border-blue-500"
+						href="https://github.com/brijr"
+					>
+						Bridger Tower
+					</a>
+					.
 				</p>
 			</Section>
 		</main>
