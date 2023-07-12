d51387f46de37cad9e0d7d597a2dd6ee5fccba19
Bridger Tower <brijr@Mini-Boi.local>
2023-07-12 15:29:01 -0600

    Updated

--- app/page.js
+++ app/page.js
@@ -11,7 +11,7 @@ import Section from "@/components/Section";
 
 export default function Home() {
   return (
-    <main className="font-light">
+    <main className="font-light transition-all">
       <Section>
         <Link href="/" className="mb-12 block">
           <Image
@@ -21,12 +21,14 @@ export default function Home() {
             className="dark:invert"
           />
         </Link>
-        <h1 className="text-3xl">Bridger Tower</h1>
-        <h2>
+        <h1 className="text-3xl opacity-75 mb-12">Bridger Tower</h1>
+        <h2 className="intro-h2">
           Designer and Developer from Salt Lake City, UT. Crafting software and
           websites using <a href="https://figma.com">Figma</a>,{" "}
-          <a href="https://nextjs.org">Next.js</a>,{" "}
-          <a href="https://reactjs.org">React</a>,{" "}
+          <a className="hover:opacity-50" href="https://nextjs.org">
+            Next.js
+          </a>
+          , <a href="https://reactjs.org">React</a>,{" "}
           <a href="https://kit.svelte.dev">SvelteKit</a>,{" "}
           <a href="https://webflow.com">Webflow</a>,{" "}
           <a href="https://framer.com">Framer</a>,{" "}
@@ -41,12 +43,12 @@ export default function Home() {
       </Section>
 
       <Section>
-        <h3 className="text-xl">Socials</h3>
+        <h3 className="text-xl opacity-75">Socials</h3>
         <div className="flex gap-2">
           {Socials.map((social, index) => (
             <a
               key={index}
-              className="transition-all border-b border-black dark:border-white hover:pb-1 hover:-mt-1 hover:border-blue-500"
+              className="transition-all border-b border-black dark:border-white hover:pb-1 hover:-mt-1 hover:border-purple-500"
               href={social.url}
               target="_blank"
               rel="nopener noreferrer"
@@ -58,44 +60,43 @@ export default function Home() {
       </Section>
 
       <Section>
-        <h3 className="text-xl">Current Ventures</h3>
+        <h3 className="text-xl opacity-75">Projects</h3>
         {Projects.map((project, index) => (
-          <a href={project.href} key={index} className="">
-            <div className="md:w-48 flex items-baseline gap-2 mb-2 mt-4">
-              <h4 className="text-lg">{project.title}</h4>
-              <p className="text-sm opacity-75">{project.linkName}</p>
-            </div>
-            <div className="w-full flex gap-2">
-              <p>&rarr;</p>
-              <p>{project.description}</p>
-            </div>
+          <a
+            key={index}
+            href={project.liveLink}
+            className="md:flex transition-all md:gap-2 justify-between items-baseline py-2 border-b hover:opacity-50 border-b-gray-600"
+          >
+            <p className="mb-2 md:mb-0" key={index}>
+              {project.title}
+            </p>
+            <p className="text-xs opacity-50 mb-2 md:mb-0">
+              {project.description}
+            </p>
           </a>
         ))}
       </Section>
 
-      <Section>
+      {/* <Section>
         <h3 className="text-xl">Playground</h3>
         <p>Coming Soon...</p>
-      </Section>
+      </Section> */}
 
-      <Section>
+      {/* <Section>
         <h3 className="text-xl">Posts</h3>
         <p>Coming Soon...</p>
-      </Section>
+      </Section> */}
 
       <Section>
-        <p>
-          Send me an email<a href="mailto:bridgertower@gmail.com">here.</a>
+        <p className="text-sm">
+          Send me an email at <em>bridger___at___9d8.dev</em>
         </p>
-        <p>
-          Created by
-          <a
-            className="transition-all border-b border-black dark:border-white hover:pb-1 hover:-mt-1 hover:border-blue-500"
-            href="https://github.com/brijr"
-          >
+        <p className="text-sm">
+          Created by{" "}
+          <a className="hover:opacity-50" href="https://github.com/brijr">
             Bridger Tower
           </a>
-          .
+          . All rights reserved.
         </p>
       </Section>
     </main>
