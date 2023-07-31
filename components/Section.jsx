5e237963242f72751a8363255c45de7e6975235b
Bridger Tower <bridgertower@gmail.com>
2023-07-31 14:47:24 -0600

    Updated

--- components/Section.jsx
+++ components/Section.jsx
@@ -1,5 +1,9 @@
 const Section = ({ children }) => {
-    return <section className="mb-24 max-w-screen-sm flex flex-col gap-4">{children}</section>;
+  return (
+    <section className="mb-24 max-w-screen-sm flex flex-col gap-4">
+      {children}
+    </section>
+  );
 };
 
 export default Section;
