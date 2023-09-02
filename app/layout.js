f07599406e574f83457fe82074a53a893958f9f0
Bridger Tower <bridgertower@gmail.com>
2023-09-02 12:30:49 -0600

    update home

--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,18 @@
-import { Manrope } from "next/font/google";
-import "./globals.css";
+import { Manrope } from 'next/font/google';
+import './globals.css';
 
-const manrope = Manrope({ subsets: ["latin"] });
+const manrope = Manrope({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "Bridger Tower | Designer and Developer",
-  description:
-    "Bridger Tower is a designer and developer from Salt Lake City, UT. Crafting software and websites using Next.js, Tailwind, and Vercel.",
+	title: 'Bridger Tower | Designer and Developer',
+	description:
+		'Bridger Tower is a designer and developer from Salt Lake City, UT. Crafting software and websites using Next.js, Tailwind, and Vercel.'
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en" className={manrope.className}>
-      <body className="p-8 md:p-24 dark:bg-[#333] dark:text-[#EEE]">
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en" className={manrope.className}>
+			<body className="p-8 md:p-24 dark:bg-[#333] dark:text-[#EEE]">{children}</body>
+		</html>
+	);
 }
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,63 +1,60 @@
-import Image from "next/image";
-import Link from "next/link";
-import Cap from "@/public/cap.svg";
-import Section from "@/components/Section";
+import Image from 'next/image';
+import Link from 'next/link';
+import Cap from '@/public/cap.svg';
+import Section from '@/components/Section';
 
 export default function Hero() {
-  return (
-    <Section>
-      <Link href="/" className="mb-12 block">
-        <Image
-          src={Cap}
-          alt="bridger tower key cap"
-          width={75}
-          className="dark:invert logo"
-        />
-      </Link>
-      <h1 className="text-4xl text-black dark:text-white transition-all hover:text-purple-500 hover:cursor-not-allowed mb-12 normal-case">
-        Bridger Tower
-      </h1>
-      <h2 className="intro-h2 text-xl">
-        Designer and Developer from Salt Lake City, UT. Crafting software and
-        websites using{" "}
-        <a className="hover:opacity-50" href="https://figma.com">
-          Figma
-        </a>
-        ,{" "}
-        <a className="hover:opacity-50" href="https://nextjs.org">
-          Next.js
-        </a>
-        ,{" "}
-        <a className="hover:opacity-50" href="https://reactjs.org">
-          React
-        </a>
-        ,{" "}
-        <a className="hover:opacity-50" href="https://kit.svelte.dev">
-          SvelteKit
-        </a>
-        ,{" "}
-        <a className="hover:opacity-50" href="https://webflow.com">
-          Webflow
-        </a>
-        ,{" "}
-        <a className="hover:opacity-50" href="https://framer.com">
-          Framer
-        </a>
-        ,{" "}
-        <a className="hover:opacity-50" href="https://tailwindcss.com">
-          Tailwind
-        </a>
-        , and{" "}
-        <a className="hover:opacity-50" href="https://vercel.com">
-          Vercel
-        </a>
-        .
-      </h2>
-      <h3>
-        As a creative technologist, I combine my passions for design and code to
-        create timeless brands, impactful digital products, and captivating
-        websites.
-      </h3>
-    </Section>
-  );
+	return (
+		<Section>
+			<Link href="/" className="mb-12 block">
+				<Image src={Cap} alt="bridger tower key cap" width={75} className="dark:invert logo" />
+			</Link>
+			<h1 className="text-4xl text-black dark:text-white transition-all hover:text-purple-500 hover:cursor-not-allowed mb-12 normal-case">
+				Bridger Tower
+			</h1>
+			<h2 className="intro-h2 text-xl">
+				Designer and Developer from Salt Lake City, UT. Crafting software and websites using{' '}
+				<a className="hover:opacity-50" href="https://figma.com">
+					Figma
+				</a>
+				,{' '}
+				<a className="hover:opacity-50" href="https://nextjs.org">
+					Next.js
+				</a>
+				,{' '}
+				<a className="hover:opacity-50" href="https://reactjs.org">
+					React
+				</a>
+				,{' '}
+				<a className="hover:opacity-50" href="https://kit.svelte.dev">
+					SvelteKit
+				</a>
+				,{' '}
+				<a className="hover:opacity-50" href="https://laravelphp.com">
+					Laravel
+				</a>
+				,{' '}
+				<a className="hover:opacity-50" href="https://webflow.com">
+					Webflow
+				</a>
+				,{' '}
+				<a className="hover:opacity-50" href="https://framer.com">
+					Framer
+				</a>
+				,{' '}
+				<a className="hover:opacity-50" href="https://tailwindcss.com">
+					Tailwind
+				</a>
+				, and{' '}
+				<a className="hover:opacity-50" href="https://vercel.com">
+					Vercel
+				</a>
+				.
+			</h2>
+			<h3>
+				As a creative technologist, I combine my passions for design and code to create timeless
+				brands, impactful digital products, and captivating websites.
+			</h3>
+		</Section>
+	);
 }
