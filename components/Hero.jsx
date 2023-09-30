56bf9cca8e9735432c37e2a39d6d0668edeee155
Bridger Tower <bridgertower@gmail.com>
2023-09-30 15:16:19 -0600

    updates to homepage

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,52 +1,15 @@
 import Image from 'next/image';
-import Link from 'next/link';
-import Cap from '@/public/cap.svg';
 import Section from '@/components/Section';
 
 export default function Hero() {
   return (
-    <Section>
-      <h1 className="text-9xl absolute top-0 left-0 text-black dark:text-white transition-all hover:text-purple-500 hover:cursor-not-allowed mb-12 normal-case">
-        Bridger Tower
-      </h1>
-      <h2 className="intro-h2 text-xl">
-        Designer and Developer from Salt Lake City, UT. Crafting software and websites using{' '}
-        <a className="hover:opacity-50" href="https://figma.com">
-          Figma
-        </a>
-        ,{' '}
-        <a className="hover:opacity-50" href="https://nextjs.org">
-          Next.js
-        </a>
-        ,{' '}
-        <a className="hover:opacity-50" href="https://reactjs.org">
-          React
-        </a>
-        ,{' '}
-        <a className="hover:opacity-50" href="https://kit.svelte.dev">
-          SvelteKit
-        </a>
-        ,{' '}
-        <a className="hover:opacity-50" href="https://laravelphp.com">
-          Laravel
-        </a>
-        ,{' '}
-        <a className="hover:opacity-50" href="https://webflow.com">
-          Webflow
-        </a>
-        ,{' '}
-        <a className="hover:opacity-50" href="https://framer.com">
-          Framer
-        </a>
-        ,{' '}
-        <a className="hover:opacity-50" href="https://tailwindcss.com">
-          Tailwind
-        </a>
-        , and{' '}
-        <a className="hover:opacity-50" href="https://vercel.com">
-          Vercel
-        </a>
-        .
+    <Section className=''>
+      <h1 className='sr-only'>Bridger Tower | Designer and Developer</h1>
+      <p className="text-6xl h-36 md:h-96 md:text-9xl flex max-w-screen md:w-screen lowercase text-black dark:text-white transition-all group">
+        <span className='inline group-hover:hidden'>Bridger Tower</span> <span className='hidden group-hover:inline'>bridger.to</span><span className='motion-reduce:animate-spin block md:inline'><Image className='md:w-12 w-8' width={100} height={100} src="/x.svg" alt='' /></span>
+      </p>
+      <h2 className="text-xl">
+        Designer and Developer from Salt Lake City, UT. Crafting software and websites.
       </h2>
       <h3>
         As a creative technologist, I combine my passions for design and code to create timeless
