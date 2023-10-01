60e77f3b06688894db0c276bdad606af8c3e9f32
Bridger Tower <bridgertower@gmail.com>
2023-10-01 14:04:43 -0600

    updates

--- app/page.js
+++ app/page.js
@@ -1,28 +1,30 @@
 // Content Imports
-import Software from '@/utils/software.json';
-import Socials from '@/utils/socials.json';
-import Websites from '@/utils/websites.json'
-import Ventures from '@/utils/ventures.json';
-import Works from '@/utils/works.json';
-import Me from '@/public/bridger.jpg'
+import Software from "@/utils/software.json";
+import Socials from "@/utils/socials.json";
+import Websites from "@/utils/websites.json";
+import Ventures from "@/utils/ventures.json";
+import Works from "@/utils/works.json";
+import Me from "@/public/bridger.jpg";
 
 // Component Imports
-import Hero from '@/components/Hero';
-import Section from '@/components/Section';
-import Image from 'next/image';
+import Hero from "@/components/Hero";
+import Section from "@/components/Section";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="font-light lowercase scroll-smooth">
       <Hero />
 
-      <Image className='mb-24 hidden md:block' src={Me} alt="pic of me" />
+      <Image className="mb-24 hidden md:block" src={Me} alt="pic of me" />
 
       {/* Ventures */}
       <Section>
         <div className="md:flex gap-2 items-baseline">
           <h3 className="text-xl opacity-75">{`// `}Ventures</h3>
-          <p className="text-sm opacity-40">Past, Present, and Future Ventures.</p>
+          <p className="text-sm opacity-40">
+            Past, Present, and Future Ventures.
+          </p>
         </div>
         {Ventures.map((venture, index) => (
           <a
@@ -31,7 +33,9 @@ export default function Home() {
             className="md:flex transition-all md:gap-2 justify-between items-baseline py-2 border-b hover:opacity-50 border-b-gray-600"
           >
             <p className="mb-2 md:mb-0">{venture.title}</p>
-            <p className="text-xs opacity-50 mb-2 md:mb-0">{venture.description}</p>
+            <p className="text-xs opacity-50 mb-2 md:mb-0">
+              {venture.description}
+            </p>
           </a>
         ))}
       </Section>
@@ -40,7 +44,9 @@ export default function Home() {
       <Section>
         <div className="md:flex gap-2 items-baseline">
           <h3 className="text-xl opacity-75">{`// `}Software</h3>
-          <p className="text-sm opacity-40">Software I have helped design and develop</p>
+          <p className="text-sm opacity-40">
+            Software I have helped design and develop
+          </p>
         </div>
         {Software.map((software, index) => (
           <a
@@ -51,7 +57,9 @@ export default function Home() {
             <p className="mb-2 md:mb-0" key={index}>
               {software.title}
             </p>
-            <p className="text-xs opacity-50 mb-2 md:mb-0">{software.description}</p>
+            <p className="text-xs opacity-50 mb-2 md:mb-0">
+              {software.description}
+            </p>
           </a>
         ))}
       </Section>
@@ -60,7 +68,9 @@ export default function Home() {
       <Section>
         <div className="md:flex gap-2 items-baseline">
           <h3 className="text-xl opacity-75">{`// `}Websites</h3>
-          <p className="text-sm opacity-40">Websites that I have designed and developed.</p>
+          <p className="text-sm opacity-40">
+            Websites that I have designed and developed.
+          </p>
         </div>
         {Websites.map((website, index) => (
           <a
@@ -71,7 +81,9 @@ export default function Home() {
             <p className="mb-2 md:mb-0" key={index}>
               {website.title}
             </p>
-            <p className="text-xs opacity-50 mb-2 md:mb-0">{website.description}</p>
+            <p className="text-xs opacity-50 mb-2 md:mb-0">
+              {website.description}
+            </p>
           </a>
         ))}
       </Section>
@@ -80,7 +92,9 @@ export default function Home() {
       <Section>
         <div className="md:flex gap-2 items-baseline">
           <h3 className="text-xl opacity-75">{`// `}Work</h3>
-          <p className="text-sm opacity-40">The places I have had the privilege of working at.</p>
+          <p className="text-sm opacity-40">
+            The places I have had the privilege of working at.
+          </p>
         </div>
         {Works.map((work, index) => (
           <a
@@ -91,7 +105,9 @@ export default function Home() {
             <p className="mb-2 md:mb-0" key={index}>
               {work.title}
             </p>
-            <p className="text-xs opacity-50 mb-2 md:mb-0">{work.description}</p>
+            <p className="text-xs opacity-50 mb-2 md:mb-0">
+              {work.description}
+            </p>
           </a>
         ))}
       </Section>
@@ -120,7 +136,7 @@ export default function Home() {
             . Send me an email at <em>bridger___at___9d8.dev</em>
           </p>
           <p className="text-sm">
-            . Created by{' '}
+            . Created by{" "}
             <a className="hover:opacity-50" href="https://github.com/brijr">
               Bridger Tower
             </a>
