6e4e9fe5fe48f265781e91484d5f654def224ad8
Bridger Tower <bridgertower@gmail.com>
2024-02-22 17:30:38 -0700

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,70 +1,62 @@
 import { Main, Section, Container } from "@/components/craft";
+import Image from "next/image";
+import cap from "@/public/cap.svg";
 import { ModeToggle } from "@/components/mode-toggle";
+import { socials } from "@/lib/info";
+import Balancer from "react-wrap-balancer";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <Main>
-      <Section className="border-b">
+      <Section className="border-b lowercase">
         <Container>
-          <h1>This is a test h1!</h1>
-          <p>Hello World!</p>
-          <ModeToggle />
-        </Container>
-      </Section>
-      <Section className="border-b">
-        <Container>
-          <h2>Subheading</h2>
-          <h3>Sub-subheading</h3>
-          <h4>Sub-sub-subheading</h4>
-          <h5>Sub-sub-sub-subheading</h5>
-          <h6>Sub-sub-sub-sub-subheading</h6>
-          <p>
-            This is a paragraph with <a href="#">an inline link</a>.
-          </p>
-          <blockquote>A blockquote would be here.</blockquote>
-          <ul>
-            <li>List item 1</li>
-            <li>List item 2</li>
-            <li>List item 3</li>
-          </ul>
-          <ol>
-            <li>Ordered item 1</li>
-            <li>Ordered item 2</li>
-            <li>Ordered item 3</li>
-          </ol>
-          <code>Code snippet</code>
-          <pre>Preformatted text</pre>
-        </Container>
-      </Section>
-      <Section className="border-b">
-        <Container>
-          <h2>This is a Heading</h2>
-          <table>
-            <thead>
-              <tr>
-                <th>Header 1</th>
-                <th>Header 2</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Data 1</td>
-                <td>Data 2</td>
-              </tr>
-              <tr>
-                <td>Data 1</td>
-                <td>Data 2</td>
-              </tr>
-              <tr>
-                <td>Data 1</td>
-                <td>Data 2</td>
-              </tr>
-              <tr>
-                <td>Data 1</td>
-                <td>Data 2</td>
-              </tr>
-            </tbody>
-          </table>
+          <div className="flex justify-between">
+            <Image
+              className="!mt-0"
+              src={cap}
+              width={50}
+              height={50}
+              alt="a key cap with a b for bridger"
+            ></Image>
+            <div className="flex gap-2 h-fit">
+              <Link href="/">posts</Link>
+              <a href="https://components.bridger.to">components</a>
+              <a href="https://design.bridger.to">design</a>
+            </div>
+          </div>
+          <h1 className="text-xl">
+            <strong>Bridger Tower</strong> is a Designer and Developer
+          </h1>
+          <div className="max-w-2xl">
+            <p>
+              I am a designer and developer from Salt Lake City, UT. I work at{" "}
+              <a href="https://ampry.com">Ampry</a> as a software engineer. I am
+              currently building{" "}
+              <a href="https://alpinecodex.com">Alpine Codex</a> and{" "}
+              <a href="https://9d8.dev">9d8</a> to deliver next-gen performance
+              marketing software.
+            </p>
+            <p>
+              I started my career as a multi-disciplinary designer before diving
+              into software development. I have a passion for design and code
+              and I aspire to enable others to learn.
+            </p>
+            <p>
+              thanks for visiting and feel free to reach out to me on any of the
+              channels below.
+            </p>
+          </div>
+          <div className="flex gap-2">
+            {socials.map((social) => (
+              <p key={social.name}>
+                <a href={social.href} target="_blank" rel="noopener noreferrer">
+                  {social.name}
+                </a>{" "}
+                \
+              </p>
+            ))}
+          </div>
         </Container>
       </Section>
     </Main>
---
+++ lib/info.ts
@@ -0,0 +1,18 @@
+export const socials = [
+  {
+    name: "GitHub",
+    href: "https://github.com/brijr",
+  },
+  {
+    name: "X (Twitter)",
+    href: "https://twitter.com/bridgertower",
+  },
+  {
+    name: "LinkedIn",
+    href: "https://www.linkedin.com/in/brdgr",
+  },
+  {
+    name: "YouTube",
+    href: "https://www.youtube.com/@bridgertower",
+  },
+];
