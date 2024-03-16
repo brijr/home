6dc89ca1f8c1150725dad991c1d53e608ad67f2b
Bridger Tower <bridgertower@gmail.com>
2024-03-16 10:31:44 -0600

    add lamp

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import * as React from "react";
-import { Moon, Sun } from "lucide-react";
+import { Lamp } from "lucide-react";
 import { useTheme } from "next-themes";
 
 import { Button } from "@/components/ui/button";
@@ -16,9 +16,9 @@ export function ModeToggle() {
   return (
     <Button variant="ghost" size="icon" onClick={toggleTheme}>
       {theme === "dark" ? (
-        <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all" />
+        <Lamp className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all" />
       ) : (
-        <Moon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all" />
+        <Lamp className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all" />
       )}
       <span className="sr-only">Toggle theme</span>
     </Button>
