8201cf1669943b52abe211e07c8b0681e6f3788a
Bridger Tower <bridgertower@gmail.com>
2024-03-16 22:44:07 -0600

    switch to mdx

--- components/portfolio-links.tsx
+++ components/portfolio-links.tsx
@@ -5,7 +5,7 @@ export const PortfolioLinks = () => {
   return (
     <div className="grid">
       <h3 className="text-foreground">Portfolio</h3>
-      <div className="flex flex-wrap gap-2">
+      <div className="flex flex-wrap gap-2 pt-2">
         {bt.categories.map((link) => (
           <Link
             className="not-prose hover:bg-accent hover:text-foreground flex items-center gap-1 rounded-md border px-2 py-px text-base transition-all"
