b0263ac4c49858ffdb9ea4d2c0bb91d151ba1b60
Bridger Tower <bridgertower@gmail.com>
2024-03-26 15:48:50 -0600

    Update CSS and page content, add redirect in next.config.js

--- next.config.js
+++ next.config.js
@@ -5,6 +5,15 @@ const nextConfig = {
   // Configure `pageExtensions` to include MDX files
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
   // Optionally, add any other Next.js config below
+  async redirects() {
+    return [
+      {
+        source: "/craft",
+        destination: "https://github.com/brijr/craft",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = withMDX(nextConfig);
