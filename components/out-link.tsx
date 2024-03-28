99bdfa3310a78f3eaa9610b86e0f5b1bf2ce8222
Bridger Tower <bridgertower@gmail.com>
2024-03-28 12:03:10 -0600

    add outlink

--- components/out-link.tsx
+++ components/out-link.tsx
@@ -1,18 +1,30 @@
-const OutLink = ({
+import { ArrowRight } from "lucide-react";
+
+export const OutLink = ({
   href,
   children,
+  desc,
 }: {
   href: string;
   children: React.ReactNode;
+  desc?: string;
 }) => {
   return (
     <a
-      className="hover:text-foreground text-muted-foreground border-b transition-all"
+      className="not-prose group flex gap-4 p-4 hover:pl-5 hover:pr-3 bg-accent/20 hover:bg-accent/50 border rounded-lg transition-all items-center my-4"
       href={href}
       target="_blank"
       rel="noopener noreferrer"
     >
-      {children}
+      <div className="w-10 h-10 border flex items-center justify-center bg-accent/50 rounded-md">
+        <ArrowRight className="-mb-px h-4 w-4 transition-all group-hover:-rotate-45" />{" "}
+      </div>
+      <div className="leading-tight">
+        <span className="flex">{children}</span>
+        {desc && (
+          <span className="block text-muted-foreground text-sm">{desc}</span>
+        )}
+      </div>
     </a>
   );
 };
