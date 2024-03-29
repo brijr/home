82a46629aaac400831cbd157248435b1b56f9ce7
Bridger Tower <bridgertower@gmail.com>
2024-03-28 21:13:14 -0600

    update portfolio links

--- components/portfolio-links.tsx
+++ components/portfolio-links.tsx
@@ -1,18 +1,23 @@
 import bt from "@/lib/brijr";
 import Link from "next/link";
+import { Folder } from "lucide-react";
 
 export const PortfolioLinks = () => {
   return (
     <div className="grid">
       <h3 className="text-foreground">Portfolio</h3>
-      <div className="flex flex-wrap gap-2 pt-2">
+      <div className="grid grid-cols-3 gap-3">
         {bt.categories.map((link) => (
           <Link
-            className="not-prose hover:text-foreground bg-accent/20 hover:bg-accent/50 flex items-center gap-1 rounded-md border px-2 py-px text-base transition-all"
+            className="not-prose hover:text-foreground bg-accent/20 hover:bg-accent/50 items-center gap-1 rounded-md border p-3 text-base transition-all flex group justify-between"
             key={link.label}
             href={link.href}
           >
             {link.label}
+            <Folder
+              size={16}
+              className="text-accent/75 group-hover:text-foreground/75 transition-all"
+            />
           </Link>
         ))}
       </div>
