d54edf2679d0f22d8a0707ee48c35f1de3d795ca
Bridger Tower <bridgertower@gmail.com>
2024-04-01 14:55:34 -0600

    posts set up

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,7 +1,30 @@
 import type { MDXComponents } from "mdx/types";
+import Image, { ImageProps } from "next/image";
+import Link from "next/link";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
+    Img: (props) => (
+      <Image
+        sizes="100vw"
+        style={{ width: "100%", height: "auto" }}
+        {...(props as ImageProps)}
+      />
+    ),
+    Date: ({ children }) => (
+      <span className="text-sm">
+        {new Date(children).toLocaleDateString("en-US", {
+          year: "numeric",
+          month: "short",
+          day: "numeric",
+        })}
+      </span>
+    ),
+    Link: (props) => (
+      <Link href={props.link} className="not-prose underline" {...props}>
+        {props.children}
+      </Link>
+    ),
     ...components,
   };
 }
