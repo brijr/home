ea0f8c039a2cb034b13e80f16e6c205caf106b6d
Bridger Tower <bridgertower@gmail.com>
2024-04-11 09:41:17 -0600

    update links to posts on mobile

--- components/date-link.tsx
+++ components/date-link.tsx
@@ -11,7 +11,7 @@ export const DateLink = ({
 }) => (
   <Link
     href={href}
-    className="text-muted-foreground transition-all hover:text-accent-foreground md:flex not-prose justify-between w-full items-center border p-4 rounded-md bg-accent/50 md:border-none md:bg-transparent md:p-0"
+    className="text-muted-foreground transition-all hover:text-accent-foreground flex not-prose justify-between w-full items-center py-2 border-b rounded-md md:border-none md:bg-transparent md:p-0"
   >
     <p className="font-normal not-prose">{name}</p>
     {year && (
