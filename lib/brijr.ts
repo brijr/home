84eee013e8c3d4ecbffcaec2f05e71fe735b7d4f
Bridger Tower <bridgertower@gmail.com>
2024-06-18 09:49:44 -0600

    update brijr.ts

--- lib/brijr.ts
+++ lib/brijr.ts
@@ -36,32 +36,32 @@ const bt = {
     {
       label: "Wavefinder",
       href: "https://wavefinder.io",
-      desc: "Message testing at scale using AI.",
+      desc: "Message testing at scale using AI",
     },
     {
       label: "BuilderKit",
       href: "https://builderkit.io",
-      desc: "Copywriting companion and smart notebook.",
+      desc: "Copywriting companion and smart notebook",
     },
     {
       label: "Outr.io",
       href: "/software/outr",
-      desc: "Automated sales outreach and follow-up.",
+      desc: "Automated sales outreach and follow-up",
     },
     {
       label: "Ishi",
       href: "https://ishi.dev",
-      desc: "Next.js starter to ship Fast.",
+      desc: "Next.js starter to ship Fast",
     },
     {
       label: "Router.so",
       href: "https://router.so",
-      desc: "Simple backend for lead capture and routing.",
+      desc: "Simple backend for lead capture and routing",
     },
     {
       label: "DocsAI",
       href: "https://docsai.org",
-      desc: "Generate SEO-rich articles, fast.",
+      desc: "Generate SEO-rich articles, fast",
     },
   ],
 
@@ -70,7 +70,12 @@ const bt = {
     {
       label: "brijr/components",
       href: "https://github.com/brijr/components",
-      desc: "Next.js components for building websites.",
+      desc: "Next.js components for building websites",
+    },
+    {
+      label: "next-wp",
+      href: "https://github.com/brijr/components",
+      desc: "Next.js starter for headless WordPress",
     },
     {
       label: "brijr/craft",
@@ -109,32 +114,32 @@ const bt = {
     {
       label: "Software Developer at Alpine Codex",
       href: "https://alpinecodex.com",
-      desc: "Developing innovative software solutions with a focus on performance and user experience.",
+      desc: "Developing innovative software solutions with a focus on performance and user experience",
     },
     {
       label: "Software Developer at 9D8",
       href: "https://9d8.dev",
-      desc: "Crafting modern web applications and services with cutting-edge technologies.",
+      desc: "Crafting modern web applications and services with cutting-edge technologies",
     },
     {
       label: "Software Developer at Ampry",
       href: "https://ampry.com",
-      desc: "Building and optimizing web platforms to enhance user engagement and conversion.",
+      desc: "Building and optimizing web platforms to enhance user engagement and conversion",
     },
     {
       label: "Web Developer at Tackle.io",
       href: "https://tackle.io",
-      desc: "Implementing scalable web architectures and collaborating on cloud-based solutions.",
+      desc: "Implementing scalable web architectures and collaborating on cloud-based solutions",
     },
     {
       label: "Product Designer at Ampry",
       href: "https://ampry.com",
-      desc: "Designing intuitive user interfaces and experiences, driving product strategy and vision.",
+      desc: "Designing intuitive user interfaces and experiences, driving product strategy and vision",
     },
     {
       label: "Designer at Zion Design",
       href: "https://zion.surf",
-      desc: "Creating visually compelling designs and branding materials for a diverse client base.",
+      desc: "Creating visually compelling designs and branding materials for a diverse client base",
     },
   ],
 
@@ -143,17 +148,17 @@ const bt = {
     {
       label: "Alpine Codex",
       href: "https://alpinecodex.com",
-      desc: "Innovative software solutions with a focus on performance and user experience.",
+      desc: "Innovative software solutions with a focus on performance and user experience",
     },
     {
       label: "9d8",
       href: "https://9d8.dev",
-      desc: "Modern web applications and services using cutting-edge technologies.",
+      desc: "Modern web applications and services using cutting-edge technologies",
     },
     {
       label: "Zion Design",
       href: "https://zion.surf",
-      desc: "Visually compelling designs and branding materials for a diverse client base.",
+      desc: "Visually compelling designs and branding materials for a diverse client base",
     },
   ],
 
@@ -162,62 +167,62 @@ const bt = {
     {
       label: "internetservices.com",
       href: "https://internetservices.vercel.app",
-      desc: "A platform offering diverse internet services and solutions.",
+      desc: "A platform offering diverse internet services and solutions",
     },
     {
       label: "ampry.com",
       href: "https://ampry.com",
-      desc: "Optimizing web platforms for enhanced user engagement and conversion.",
+      desc: "Optimizing web platforms for enhanced user engagement and conversion",
     },
     {
       label: "wavefinder.io",
       href: "https://wavefinder.io",
-      desc: "A tool for surfers to find the best waves and surf conditions.",
+      desc: "A tool for surfers to find the best waves and surf conditions",
     },
     {
       label: "builderkit.io",
       href: "https://builderkit.io",
-      desc: "Providing developers with tools and kits for building web applications.",
+      desc: "Providing developers with tools and kits for building web applications",
     },
     {
       label: "outr.io",
       href: "https://outr.io",
-      desc: "Automated cold outreach and follow-up platform for sales teams.",
+      desc: "Automated cold outreach and follow-up platform for sales teams",
     },
     {
       label: "asap.engineering",
       href: "https://asap.engineering",
-      desc: "Engineering studio for software.",
+      desc: "Engineering studio for software",
     },
     {
       label: "pandium.com",
       href: "https://pandium.com",
-      desc: "Integrations and automations for SaaS applications to streamline business processes.",
+      desc: "Integrations and automations for SaaS applications",
     },
     {
       label: "travelmellow.com",
       href: "https://travelmellow.com",
-      desc: "A travel platform for planning and booking serene getaways.",
+      desc: "A travel platform for planning and booking serene getaways",
     },
     {
       label: "sondrmarketing.com",
       href: "https://sondrmarketing.com",
-      desc: "A marketing agency specializing in creating compelling designs and branding strategies.",
+      desc: "Marketing agency specializing in assited living and senior care",
     },
     {
       label: "allveteran.com",
       href: "https://allveteran.com",
-      desc: "A resource and community platform dedicated to veterans.",
+      desc: "A resource and community platform dedicated to veterans",
     },
     {
       label: "autobyte.dev",
       href: "https://autobyte.dev",
-      desc: "Automotive technology solutions and development services.",
+      desc: "Automotive technology solutions and development services",
     },
     {
       label: "modelofconfidence.com",
       href: "https://modelofconfidence.com",
-      desc: "A platform promoting self-confidence and personal growth.",
+      desc: "A platform promoting self-confidence and personal growth",
     },
   ],
 };
